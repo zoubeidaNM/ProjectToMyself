f74b9af5f28d6729810ae66ab4f0ae58cbccf853
zoubeidaNM <zoumenz@yahoo.com>
2017-10-02 14:38:03 -0400

    This is Project to your self assignement

---
+++ src/ProjectToMyself.java
@@ -0,0 +1,10 @@
+public class ProjectToMyself {
+    public static void main(String args[]) {
+        // this will tell you my name and age
+        // Print out name
+        System.out.println("My name is Zoubeida");
+        // Print out age
+        System.out.println("My age is more then 40");
+    }
+
+}
